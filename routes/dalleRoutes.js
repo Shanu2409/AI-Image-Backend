cafcd829cc4041470dbfd09f266b2e60ecc39bfb
shanu2409 <shanusingh2409@gmail.com>
2023-05-27 01:25:09 +0530

    first

---
+++ routes/dalleRoutes.js
@@ -0,0 +1,39 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL-E!" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.createImage({
+      prompt: "a white siamese cat",
+      n: 1,
+      size: "1024x1024",
+    });
+
+    const image = response.data.data[0].url;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
+});
+
+export default router;
